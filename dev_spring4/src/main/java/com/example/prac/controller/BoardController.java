fd200ea5bd1ab9bca4a1a01112b0a56ea692a8bf
melll93 <yemeltu@naver.com>
2023-03-09 15:16:01 +0900

    spring migration

---
+++ dev_spring4/src/main/java/com/example/prac/controller/BoardController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.example.demo.logic.BoardLogic;
+import com.example.demo.util.HashMapBinder;
+
+//왜 나만 상속을 받아야 하나요? - 
+public class BoardController extends MultiActionController {
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	private BoardLogic boardLogic = null;
+	//setter객체주입법 - 자바코드에서 객체주입을 처리할 때 사용함
+	public void setBoardLogic(BoardLogic boardLogic) {
+		this.boardLogic = boardLogic;
+	}
+	//void doGet(req,res)-> ActionForward execute(req,res) -> String execute(req,res) -> String XXXX(req,res)
+	public ModelAndView boardList(HttpServletRequest req, HttpServletResponse res)
+	{
+		logger.info("boardList호출");
+		List<Map<String,Object>> bList = null;
+		Map<String, Object> pMap = new HashMap<>();
+		HashMapBinder hmb = new HashMapBinder(req);
+		hmb.bind(pMap);
+		bList = boardLogic.boardList(pMap);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("board3/boardList");
+		mav.addObject("bList", bList);
+		//
+		return mav;
+	}
+}
---
+++ dev_spring4/src/main/java/com/example/prac/util/HashMapBinder.java
@@ -0,0 +1,32 @@
+package com.example.demo.util;
+
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+//Spring 부트에서는 RequestParam 대신 해줌, Model , ModelMap
+//사용자가 입력한 값을 Map 에 담아 준다
+//담을 Map은 컨트롤계층에서 bind메소드 호출시 파라미터를 이용해서 원본 주소번지를 받아온다.
+//그리고 그 안에 담는다.
+public class HashMapBinder {
+	HttpServletRequest req = null;//전변
+	public HashMapBinder() {}
+	//생성자 파라미터에 요청객체(지변)가 필요한 이유는 뭐죠?
+	public HashMapBinder(HttpServletRequest req) {
+		//생성자의 1역할 - 전변의 초기화
+		this.req = req;
+	}
+	//어떤 페이지 어떤 상황에서 공통코드 재사용 가능하게 할 것인가?
+	//업무별 요청 클래스에서 주입 받을 객체를 정해서 메소드의 파라미터로 전달 받음
+	//전달받은 주소번지 원본에 값을 담아준다
+	public void bind(Map<String,Object> pMap) {
+		pMap.clear();
+		Enumeration<String> en = req.getParameterNames();
+		while(en.hasMoreElements()) {
+			String key = en.nextElement();
+			pMap.put(key, req.getParameter(key));
+		}
+	}
+}
+
