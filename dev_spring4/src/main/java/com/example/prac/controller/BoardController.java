2a9b7c7380428185fa2192ab2fdfcc4e3c4719c6
melll93 <yemeltu@naver.com>
2023-03-10 17:35:09 +0900

    xml

--- dev_spring4/src/main/java/com/example/prac/controller/BoardController.java
+++ dev_spring4/src/main/java/com/example/prac/controller/BoardController.java
@@ -16,11 +16,11 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 import com.example.prac.logic.BoardLogic;
 import com.example.prac.util.HashMapBinder;
 
-//왜 나만 상속을 받아야 하나요? - 
+//�솢 �굹留� �긽�냽�쓣 諛쏆븘�빞 �븯�굹�슂? - 
 public class BoardController extends MultiActionController {
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	private BoardLogic boardLogic = null;
-	//setter객체주입법 - 자바코드에서 객체주입을 처리할 때 사용함
+	//setter媛앹껜二쇱엯踰� - �옄諛붿퐫�뱶�뿉�꽌 媛앹껜二쇱엯�쓣 泥섎━�븷 �븣 �궗�슜�븿
 	public void setBoardLogic(BoardLogic boardLogic) {
 		this.boardLogic = boardLogic;
 	}
